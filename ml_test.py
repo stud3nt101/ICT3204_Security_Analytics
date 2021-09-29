483d214e47e3a71c23bf9b0261442af72f321931
CB-Hong <1902164@sit.singaporetech.edu.sg>
2021-09-29 15:07:42 +0800

    update

--- ml_test.py
+++ ml_test.py
@@ -1,4 +1,9 @@
-import xgboost as xgb
+import pandas as pd
+import numpy as np
 import os
+import datetime as dt
 
-os.system
+from pandas.tools.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+
