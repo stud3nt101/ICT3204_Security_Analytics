8296e9149981c5041d484511e50938e36c105f4f
abc <abc@customName.asd>
2021-10-16 10:27:38 -0700

    updated and split function

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 import json
 import logs
 import LoadData
+from os import system 
 
 from flask import Flask, render_template, redirect, request, make_response
 
@@ -37,4 +38,42 @@ def histlog():
 # Route for upload page
 @app.route('/upload')
 def upload():
+    pcap_file = request.files['pcap']
+    log_file = request.files['file']
+    log_file2 = request.files['file2']
+    
+    print("hello")
+    # pcap save
+    if pcap_file.filename != '':
+        
+        pcap_file.save("upload/"+pcap_file.filename)
+
+        # require tshark
+        # require argus-server
+        # saved in a folder called upload(change name if needs be) 
+        system("tshark -r upload/" + pcap_file.filename + " -T fields -E header=y -E separator=, -E occurrence=a -E quote=d" 
+        " -e frame.time -e _ws.col.Protocol -e _ws.col.Length -e tcp.flags -e ip.src -e tcp.srcport -e udp.srcport -e ip.dst" 
+        " -e tcp.dstport -e udp.dstport -e _ws.col.Info > upload/temp.csv")
+
+        #precreate an empty asd.biargus file, idk why also, but kk say do it
+        system("argus -F utils/argus.conf -r upload/" + pcap_file.filename + " -w temp.biargus | ra -r temp.biargus -n -F utils/ra.conf -Z b > upload/temp.binetflow")
+
+    # log file 1 save
+    print(log_file.filename)
+    if log_file.filename != '':
+        log_file.save("upload/"+log_file.filename)
+
+    # log file 2 save
+    if log_file2.filename != '':
+        log_file2.save("upload/"+log_file2.filename)
+
     return render_template('upload.html')
+
+'''
+<form action = "/json" method = "POST" enctype = "multipart/form-data">
+    <input type = "file" name = "pcap" />
+    <input type = "file" name = "file" />
+    <input type = "file" name = "file2" />
+    <input type = "submit"/>
+</form>
+'''
