c88881f3df3ef4f5c316c577e64819f57440574b
unknown <stoneisfun@gmail.com>
2021-10-17 20:16:26 +0800

    updated analysis

--- app/pcap.py
+++ app/pcap.py
@@ -14,15 +14,19 @@ import pandas as pd
 
 '''
 
-def pcap(filname):
+def pcap_analysis(filname):
     analysis_list=[]
     df = pd.read_csv (filname,encoding= 'utf-8',quotechar="'", parse_dates=[0])
-    
+
     proto_counts = df["_ws.col.Protocol"].value_counts() # Number of protocol
 
     tcp_flag_counts = df["tcp.flags"].value_counts() # Number of tcp flags
 
     interval_count = df.resample('10Min', on='frame.time').agg({'frame.time': 'count'}).rename_axis('time') # Interval of ip addr
+
+    # Converting main df to dict to be outputted into pagination table
+    df['frame.time']=df['frame.time'].dt.strftime('%Y-%m-%d %r')
+    table_dict = df.to_dict('records')
 
     ip_src_counts = df["ip.src"].value_counts() # Number of src ip addr
     ip_dst_counts = df["ip.dst"].value_counts() # Number of dst ip addr
@@ -36,158 +40,47 @@ def pcap(filname):
 
     pkt_length_counts = df["_ws.col.Length"].value_counts() # Number of packet length
 
-    # Converting main df to dict to be outputted into pagination table
-    table_dict = df.to_dict('records')
-
     # Preping the respective df to be converted to dict 
-    # datastructure --> [[{column1:data1, column2:data2}]] (refer to sample html code below for sample usage)
+    # datastructure --> [{key1:value1, key2:value2}, {key3:value3, key4:value4}]
     proto_counts_dict = proto_counts.reset_index()
     proto_counts_dict.columns = ['Protocol','Protocol_Count']
-    analysis_list.append(proto_counts_dict.to_dict('records'))
-    
+    analysis_list.append(dict(proto_counts_dict.values))
+
     tcp_flag_dict = tcp_flag_counts.reset_index()
     tcp_flag_dict.columns = ['TCP_Flags','TCP_Flags_Count']
-    analysis_list.append(tcp_flag_dict.to_dict('records'))
+    analysis_list.append(dict(tcp_flag_dict.values))
 
     interval_count_dict = interval_count.reset_index()
     interval_count_dict.columns =['Time','Time_Count']
-    analysis_list.append(interval_count_dict.to_dict('records'))
+    interval_count_dict['Time']=interval_count_dict['Time'].dt.strftime('%Y-%m-%d %r')
+    analysis_list.append(dict(interval_count_dict.values))
 
     ip_src_counts_dict = ip_src_counts.reset_index()
     ip_src_counts_dict.columns =['Src_IP_Addr','IP_Addr_Count']
-    analysis_list.append(ip_src_counts_dict.to_dict('records'))
+    analysis_list.append(dict(ip_src_counts_dict.values))
 
     ip_dst_counts_dict = ip_dst_counts.reset_index()
     ip_dst_counts_dict.columns =['Dst_IP_Addr','IP_Addr_Count']
-    analysis_list.append(ip_dst_counts_dict.to_dict('records'))
+    analysis_list.append(dict(ip_dst_counts_dict.values))
 
-    total_packet_dict = [{"Packets":total_packet}]
+    total_packet_dict = {"Packets":total_packet}
     analysis_list.append(total_packet_dict)
 
-    unique_combine_dict = [{"Unique_IP":unique_combine}]
+    unique_combine_dict = {"Unique_IP":unique_combine}
     analysis_list.append(unique_combine_dict)
 
     ip_src_counts_dict = ip_src_counts.reset_index()
     ip_src_counts_dict.columns =['Src_IP_Addr','IP_Addr_Count']
-    analysis_list.append(ip_src_counts_dict.to_dict('records'))
+    analysis_list.append(dict(ip_src_counts_dict.values))
 
-    # unique_src_dict = unique_src.reset_index()
-    # unique_src_dict.columns =['Unique_Src_IP_Addr','IP_Addr_Count']
-    # analysis_list.append(unique_src_dict.to_dict('records'))
-    #
-    # unique_dst_dict = unique_dst.reset_index()
-    # unique_dst_dict.columns =['Unique_Dst_IP_Addr','IP_Addr_Count']
-    # analysis_list.append(unique_dst_dict.to_dict('records'))
+    unique_src_dict = {"Unique_Src_IP":unique_src}
+    analysis_list.append(unique_src_dict)
+
+    unique_dst_dict = {"Unique_Dst_IP":unique_dst}
+    analysis_list.append(unique_dst_dict)
 
     pkt_length_counts_dict = pkt_length_counts.reset_index()
     pkt_length_counts_dict.columns =['Packets_Length','Packets_Length_Count']
-    analysis_list.append(pkt_length_counts_dict.to_dict('records'))
+    analysis_list.append(dict(pkt_length_counts_dict.values))
 
     return analysis_list, table_dict
-
-'''
-------------------
-Sample html usage|
------------------
-
-{%if ctrsuccess %}
-<div class="table-responsive">
-    <table class="table table-striped">
-        <tbody>
-            <h1>Protocol</h1>
-            {% for dict_item in ctrsuccess[0]%}
-                
-                </tr>
-                    <td>{{ dict_item['Protocol'] }} : {{ dict_item['Protocol_Count'] }}</td>
-                </tr>
-               
-            {% endfor %}
-        </tbody>
-    </table>
-    <table>
-        <tbody>
-            <h1>TCP_FLag</h1>
-            {% for x in ctrsuccess[1]%}
-                
-                </tr>
-                    <td>{{ x['TCP_Flags'] }} : {{ x['TCP_Flags_Count'] }}</td>
-                </tr>
-               
-            {% endfor %}
-        </tbody>
-    </table>
-    <table>
-        <tbody>
-            <h1>Interval</h1>
-            {% for x in ctrsuccess[2]%}
-                
-                </tr>
-                    <td>{{ x['Time'] }} : {{ x['Time_Count'] }}</td>
-                </tr>
-               
-            {% endfor %}
-        </tbody>
-    </table>
-    <table>
-        <tbody>
-            <h1>Occurance of Unique IP Address</h1>
-            {% for x in ctrsuccess[3]%}
-                
-                </tr>
-                    <td>{{ x['IP_Addr'] }} : {{ x['IP_Addr_Count'] }}</td>
-                </tr>
-               
-            {% endfor %}
-        </tbody>
-    </table>
-    <table>
-        <tbody>
-            <h1>Total Packet</h1>
-            {% for x in ctrsuccess[4]%}
-                
-                </tr>
-                    <td>{{ x['Packets'] }}</td>
-                </tr>
-               
-            {% endfor %}
-        </tbody>
-    </table>
-    <table>
-        <tbody>
-            <h1>Total Number of unique IP</h1>
-            {% for x in ctrsuccess[5]%}
-                
-                </tr>
-                    <td>{{ x['Unique_IP'] }}</td>
-                </tr>
-               
-            {% endfor %}
-        </tbody>
-    </table>
-    <table>
-        <tbody>
-            <h1>Occurace of Packet Length</h1>
-            {% for x in ctrsuccess[6]%}
-                
-                </tr>
-                    <td>{{ x['Packets_Length'] }} : {{ x['Packets_Length_Count'] }}</td>
-                </tr>
-               
-            {% endfor %}
-        </tbody>
-    </table>
-    <table>
-        <tbody>
-            <h1>table</h1>
-            {% for x in table%}
-                
-                </tr>
-                    <td>{{ x['frame.time'] }}</td> <--- replace with relevant field name as needed (referenced from the tshark command above) 
-                </tr>
-               
-            {% endfor %}
-        </tbody>
-    </table>
-</div>
-{%endif%}
-'''  
